cf27c9cd793864c7fcc61dc8d79d0e53211db2e8
Martin Helmut Fieber <info@martin-fieber.se>
2021-06-13 14:08:21 +0200

    Fix sha256 hash

--- Formula/litr.rb
+++ Formula/litr.rb
@@ -2,7 +2,7 @@ class Litr < Formula
   desc "Litr (Language Independent Task Runner) lets you configure and then run any tasks you want for any language."
   homepage "https://github.com/krieselreihe/litr"
   url "https://github.com/krieselreihe/litr/releases/download/v1.0.0-alpha.1/litr-mac-1.0.0-alpha.1.tar.gz"
-  sha256 "31a59c773aed589485becee416337ce0915c6b628103a96cb7395e968adb43f2"
+  sha256 "ec673df548d244b69d8681d7474563fca7f921387b4c064b1da1fb620b8e1f7d"
   version "1.0.0-alpha.1"
 
   def install
