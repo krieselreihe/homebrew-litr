0b124eba419360873c17a4db9e2654de7004004c
Martin Helmut Fieber <info@martin-fieber.se>
2022-08-02 22:50:10 +0200

    Release v1.0.0-alpha.6

--- Formula/litr.rb
+++ Formula/litr.rb
@@ -1,9 +1,9 @@
 class Litr < Formula
   desc "Litr (Language Independent Task Runner) lets you configure and then run any tasks you want for any language."
   homepage "https://github.com/krieselreihe/litr"
-  url "https://github.com/krieselreihe/litr/releases/download/v1.0.0-alpha.4/litr-mac-1.0.0-alpha.4.tar.gz"
-  sha256 "effb5cfd60d3493d70b1696a169aaf4c091e938e05eb91df056a4c3f9773f6ff"
-  version "1.0.0-alpha.4"
+  url "https://github.com/krieselreihe/litr/releases/download/v1.0.0-alpha.6/litr-mac-1.0.0-alpha.6.tar.gz"
+  sha256 "38608a67802257a0038f3141ba6239054cac4927b9110f1ba3a7cf8e1c53fe6a"
+  version "1.0.0-alpha.6"
 
   def install
     bin.install "litr"
